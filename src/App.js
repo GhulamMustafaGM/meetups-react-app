ec9921bf4e5a6d4b842a79924aa0ffe378ca15a5
Ghulam Mustafa <mustafaji@gmail.com>
2021-03-18 01:15:39 +0100

    updated My Todo List App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-
+import Todo from './components/Todo';
 
 function App() {
   return (
     <div>
-      <h1>My Todo List App</h1>
-      <div className='card'>
-        <h2>Title</h2>
-        <div className='actions' >
-          <button className='btn'>Delete</button>
-        </div>
-      </div>
+      <h1>My Todos</h1>
+      <Todo text='Learn React' />
+      <Todo text='Master React' />
+      <Todo text='Explore the full React course' />
     </div>
   );
 }
